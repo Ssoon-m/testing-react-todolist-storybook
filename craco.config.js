da29b5950d4eba0c41c91f19e8fe449c2ff144aa
권순민 <tkksm2@gmail.com>
2022-12-21 17:24:50 +0900

    add @svgr/webpack & url-loader

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,16 @@
 const CracoAlias = require('craco-alias');
 
 module.exports = {
+  webpack: {
+    configure: (config, { env, paths }) => {
+      config.module.rules.unshift({
+        test: /\.svg$/,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack', 'url-loader'],
+      });
+      return config;
+    },
+  },
   plugins: [
     {
       plugin: CracoAlias,

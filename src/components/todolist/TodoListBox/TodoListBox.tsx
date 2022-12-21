44b2439d0030a5ce906309900d677cc7e79d1c56
순민 <tkksm2@gmail.com>
2022-12-21 23:41:14 +0900

    feat: 날짜 포맷에 Intl 사용

--- src/components/todolist/TodoListBox/TodoListBox.tsx
+++ src/components/todolist/TodoListBox/TodoListBox.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import TodoList from '../TodoList';
 import TodoInputBox from '../TodoInput/TodoInputBox';
 import * as S from './TodoListBox.style';
+
+const dayOptions = {
+  weekday: 'long',
+  month: 'short',
+  day: 'numeric',
+} as const;
+
 const TodoListBox = () => {
   return (
     <S.Container>
       <div>
         <h2>Hello, Soonmin</h2>
-        <h3>It's Wednesday, 21</h3>
+        <h3>{`It's ${new Intl.DateTimeFormat('en-us', dayOptions).format(
+          new Date(),
+        )}`}</h3>
       </div>
       <TodoInputBox />
       <TodoList />
